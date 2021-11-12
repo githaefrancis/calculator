b72f2d3d2fcf598f7ca062eb2cd9b15f75b44419
Francis Githae <francis.githae@student.moringaschool.com>
2021-11-12 06:41:23 +0300

    initial commit

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+let add=(number1,number2)=>number1 + number2;
+
+let subtract=(number1,number2)=>number1-number2;
+
+let number1=parseInt(prompt("Enter a number:"));
+
+let number2=parseInt(prompt("Enter another number"));
+
+
